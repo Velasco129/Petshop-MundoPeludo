8568608ceb37890efb70ed3cf5bab55525be5ef7
Lucas Velasco <lucas.velasco22@gmail.com>
2024-05-15 17:57:42 -0300

    version 1.0.2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+let meta=`<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>PetShop Nombres</title>
+<link rel="stylesheet" href="./css/Estilos.css"><!--Hoja Stilo-->
+<link rel="icon" href="./img/mascota icono.png"><!--Favicon-->
+<link rel="preconnect" href="https://fonts.googleapis.com"><!--fuente de googlefont-->
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https: //fonts.googleapis.com/css2?family=Radio+Canada+Big:ital,wght@0,400..700;1,400..700&display=swap"
+    rel="stylesheet">`
+
+document.querySelector("head").innerHTML=meta
+
 let navegacion=`
 <div class="logo">
 <a href="./Index.html"><img src="./img/Logo.jpg" alt="logode la marca" ></a> 
