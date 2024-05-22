5c667afad0cd717a977abaf984b3dc53b2870a1e
Lucas Velasco <lucas.velasco22@gmail.com>
2024-05-22 13:19:16 -0300

    version 1.3.0

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-let meta=`<meta charset="UTF-8">
+let meta = `  <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Mundo peludo</title>
 <link rel="stylesheet" href="./css/Estilos.css"><!--Hoja Stilo-->
-<link rel="icon" href="./img/mascota icono.png"><!--Favicon-->
-<link rel="preconnect" href="https://fonts.googleapis.com"><!--fuente de googlefont-->
+<link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https: //fonts.googleapis.com/css2?family=Radio+Canada+Big:ital,wght@0,400..700;1,400..700&display=swap"
-    rel="stylesheet">`
+<link href="https://fonts.googleapis.com/css2?family=Anton&family=Lilita+One&display=swap" rel="stylesheet">`
 
-let navegacion=`
+let navegacion = `
 <div class="logo">
 <a href="./Index.html"><img src="./img/Logo.jpg" alt="logode la marca" ></a> 
 </div>
@@ -22,14 +20,26 @@ let navegacion=`
 </nav>
 <a href="./Registro.html" class="btn"><button>Contactenos</button></a>
 `
-let pie=`
-<p>Sitio desarrollado en codo a codo</p>
-        <a href="https://www.facebook.com/naturallifepet"><img src="./img/facebook.png" alt=""></a>
+let navegacion2 = `<div class="logo">
+<a href="./Index.html"><img src="./img/Logo.jpg" alt="logode la marca" ></a> 
+</div>
+<nav class="nav">
+<a href="./Productos.html">Productos</a>
+<a href="./Servicios.html">Servicios</a>
+<a href="./Sucursales.html">Sucursales</a>
+<a href="./Nosotros.html">Nosotros</a>
+<a href="./Registro.html">Contactenos</a>
+</nav>`
+
+let pie = `<h2>Seguinos en las redes</h2>
+ <a href="https://www.facebook.com/naturallifepet"><img src="./img/facebook.png" alt=""></a>
         <a href="https://www.instagram.com/natural_life_pet/"><img src="./img/instagram.png" alt=""></a>
         <a href="https://api.whatsapp.com/send?phone=5491167026320"><img src="./img/whatsapp.png" alt=""></a>
-        <p>Autores: Velasco Lucas - Santiago Kelly - Juan Parnas - Marcos Gimenez</p>
+<p>Sitio desarrollado en codo a codo</p>
+       
+        
 `
 
-document.querySelector("head").innerHTML=meta
-document.querySelector("header").innerHTML=navegacion
-document.querySelector("footer").innerHTML=pie
+document.querySelector("head").innerHTML = meta
+document.querySelector("header").innerHTML = navegacion2
+document.querySelector("footer").innerHTML = pie
